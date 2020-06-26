ac8429845ab673bd84766d2d83495cdd35b46280
Cẩm Huỳnh <huynhquancam@gmail.com>
2020-06-26 20:27:59 +0200

    Upgrade hackney

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule JobBoard.MixProject do
 
   defp deps() do
     [
-      {:hackney, "== 1.6.5"},
+      {:hackney, "~> 1.15.0"},
       {:jason, "~> 1.1"},
       {:dicon, "~> 0.5.0", runtime: false},
       {:distillery, "~> 2.0", runtime: false},
